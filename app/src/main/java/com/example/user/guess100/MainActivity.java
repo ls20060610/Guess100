656b13d095e5a1a2da397f6bf04d8c425da619f6
ls20060610 <a0930325385@gmail.com>
2017-11-14 10:10:14 +0800

    findViews

--- app/src/main/java/com/example/user/guess100/MainActivity.java
+++ app/src/main/java/com/example/user/guess100/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.user.guess100;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       findviews();
     }
+    private void findviews(){
+        TextView edsecert = (TextView) findViewById(R.id.secert);
+        TextView edinput = (TextView) findViewById(R.id.info);
+        EditText ednumber = (EditText) findViewById(R.id.number);
+        Button edsend = (Button) findViewById(R.id.send);
+    }
+
 }
